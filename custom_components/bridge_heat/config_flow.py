d760b8a2e93b8dd1f522f2fb4ced75dda205ecc2
kenzo-bento <notkenneth57@gmail.com>
2026-06-29 11:00:10 -0700

    Enhance config flow with new environmental options
    Added options for additional environmental variables in the configuration flow.

--- custom_components/bridge_heat/config_flow.py
+++ custom_components/bridge_heat/config_flow.py
@@ -3,7 +3,7 @@ from homeassistant import config_entries
 import voluptuous as vol
 from .const import *
 
-# Setting up User Permissions, modify as needed
+# Modify environmental variables here
 PERMS_SCHEMA = vol.Schema({
     vol.Optional(TEMP, default=False): bool,
     vol.Optional(HUMIDITY, default=False): bool,
@@ -33,6 +33,59 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     def async_get_options_flow(config_entry):
         return OptionsFlowHandler()
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+
+    async def async_step_init(self, user_input=None):
+        return await self.async_step_options()
+
+    async def async_step_options(self, user_input=None):
+        if user_input is None:
+            # Modify environmental variables here
+            return self.async_show_form(
+                step_id="options",
+                data_schema=vol.Schema({
+                    vol.Optional(
+                        TEMP,
+                        default=self.config_entry.options.get(TEMP, False),
+                    ): bool,
+                    vol.Optional(
+                        HUMIDITY,
+                        default=self.config_entry.options.get(HUMIDITY, False),
+                    ): bool,
+                    vol.Optional(
+                        PRESSURE,
+                        default=self.config_entry.options.get(PRESSURE, False),
+                    ): bool,
+                    vol.Optional(
+                        LIGHT,
+                        default=self.config_entry.options.get(LIGHT, False),
+                    ): bool,
+                    vol.Optional(
+                        HVAC,
+                        default=self.config_entry.options.get(HVAC, False),
+                    ): bool,
+                    vol.Optional(
+                        ENERGY,
+                        default=self.config_entry.options.get(ENERGY, False),
+                    ): bool,
+                    vol.Optional(
+                        NOISE,
+                        default=self.config_entry.options.get(NOISE, False),
+                    ): bool,
+                    vol.Optional(
+                        AQ,
+                        default = self.config_entry.options.get(AQ, False),
+                    ): bool,
+                    #add more permissions here
+                }),
+            )
+
+        return self.async_create_entry(
+            title="",
+            data=user_input,
+        )
+
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
 
