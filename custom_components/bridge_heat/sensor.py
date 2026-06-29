a2b658caa41b67d797beeae171ed4c2005f1dd87
kenzo-bento <notkenneth57@gmail.com>
2026-06-29 11:00:38 -0700

    Refactor BridgeHeat to BridgeHeatSensor class

--- custom_components/bridge_heat/sensor.py
+++ custom_components/bridge_heat/sensor.py
@@ -1,17 +1,35 @@
 from homeassistant.helpers.entity import Entity
 
+from .const import DOMAIN
+
+# Function to read the status of the integration
 async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([BridgeHeat()])
+    async_add_entities([BridgeHeatSensor(hass, entry)])
 
 
-class BridgeHeat(Entity):
-    _attr_name = "Bridge Heat"
-    _attr_unique_id = "bridge_heat"
+class BridgeHeatSensor(Entity):
+    def __init__(self, hass, entry):
+        self.hass = hass
+        self.entry = entry
+
+        self._attr_name = "Bridge Heat"
+        self._attr_unique_id = "bridge_heat"
 
     @property
     def state(self):
-        state = self.hass.states.get("weather.forecast_home")
-        if state is None:
-            return "No Data"
+        data = self.hass.data[DOMAIN][self.entry.entry_id]
+        return data.get("status", "Unknown")
 
-        return "Collecting Data"
+    @property
+    def extra_state_attributes(self):
+        data = self.hass.data[DOMAIN][self.entry.entry_id]
+
+        return {
+            "samples": len(data.get("samples", [])),
+            "last_upload": data.get("last_upload"),
+            "last_error": data.get("last_error"),
+        }
+
+    @property
+    def should_poll(self):
+        return False
