8626a3d98fe7add521fb548eaacdd6f166f6d5ef
labgeea <labgeea@gmail.com>
2026-09-10 09:52:34 -0400

    Update const.py
    updated URL

--- custom_components/bridge_heat/const.py
+++ custom_components/bridge_heat/const.py
@@ -3,7 +3,7 @@ PLATFORMS = ["sensor"]
 
 UPLOAD_INTERVAL = 60 * 24 * 60 # seconds
 SAMPLE_INTERVAL = 60 * 24 * 60
-URL = "https://rupvzdyytqxmpplraztt.supabase.co/rest/v1/bridgeheat"
+URL = "https://bridgeheat.mssm.edu/api/v1/env"
 KEY = "sb_secret_4jlAjWFgrbVh44Y_nkOvYw_Wj5D_gfU"
 
 TEMP = "Temperature"
